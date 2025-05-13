71584bbffe3dad16adbbc2e798b4bbd3522954ba
Rabin Rai <137150382+rabin2059@users.noreply.github.com>
2025-05-13 23:07:50 +0545

    Update notificationHub.js

--- bookLet-frontend/src/components/user/notificationHub.js
+++ bookLet-frontend/src/components/user/notificationHub.js
@@ -4,7 +4,7 @@ let connection = null;
 
 export const startSignalRConnection = async (onMessageReceived) => {
   connection = new HubConnectionBuilder()
-    .withUrl("https://f2h32s33-5202.inc1.devtunnels.ms/hubs/notification")
+    .withUrl("https://booklett-production.up.railway.app/hubs/notification")
     .configureLogging(LogLevel.Information)
     .withAutomaticReconnect()
     .build();
