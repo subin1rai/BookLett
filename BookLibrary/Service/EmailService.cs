0854af77d25efa42ab9d49838943fa34fe5d45f6
subin1rai <bantawasubin@gmail.com>
2025-05-13 13:44:59 +0545

    email configured2

--- BookLibrary/Service/EmailService.cs
+++ BookLibrary/Service/EmailService.cs
@@ -14,13 +14,12 @@ public class EmailServices : IEmailService
 
     public async Task SendEmail(string receptor, string subject, string body)
     {
-        var apiKey = "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";
+        var apiKey = _config["SendGrid:ApiKey"];
         var client = new SendGridClient(apiKey);
 
         var from = new EmailAddress("onsiteapp.np@gmail.com", "Booklett");
         var to = new EmailAddress(receptor);
         var msg = MailHelper.CreateSingleEmail(from, to, subject, body, body);
- 
         var response = await client.SendEmailAsync(msg);
 
         Console.WriteLine($"SendGrid Status: {response.StatusCode}");
